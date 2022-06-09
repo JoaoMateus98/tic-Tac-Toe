39d1124b38f90f9e612f13669e61b38155358105
JoaoMateus98 <joaobsu98@gmail.com>
2022-06-09 16:03:18 -0400

    created the 3 objects needed for the game

---
+++ script.js
@@ -0,0 +1,14 @@
+const log = (...args) => console.log(...args);
+
+const gameBoard = (() => {
+    const startButton = document.querySelector('.start-button');
+    
+})();
+
+const displayController = (() => {
+
+})();
+
+const player = (name) => {
+
+}
