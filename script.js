5e3d613712419f6bfa564d7a2565e64c975205b6
JoaoMateus98 <joaobsu98@gmail.com>
2022-06-09 19:37:09 -0400

    gameboard updates based on position of clicked cell

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const log = (...args) => console.log(...args);
 let playerOne;
 let playerTwo;
+let gameBoard = [['top-left', 'top-mid', 'top-right'],
+                 ['mid-left', 'mid-mid', 'mid-right'],
+                 ['low-left', 'low-mid', 'low-right']];
+let currentPlayer = 'x';
 
 const DomGrabber = (() => {
     const startGameContainer = document.querySelector('.start-game-container');
     const gameContainer = document.querySelector('.game-container');
     const startButton = document.querySelector('.start-button');
+    const boardCells = document.querySelectorAll('.board-cell');
     const form = document.querySelector('form');
 
     return {
         startButton,
         form,
         startGameContainer,
-        gameContainer
+        gameContainer,
+        boardCells
     }
 })();
 
 const GameBoard = (() => {
+    DomGrabber.boardCells.forEach((cell) => { // adds Xs and Os to the gameboard array
+        cell.addEventListener('click', () => {
+            let currentRow = -1;
+            let currentColumn = -1;
+            gameBoard.forEach((row) => {
+                currentRow++;
+                currentColumn = -1;
+                row.forEach((column) => {
+                    currentColumn++;
+                    if (cell.id === column) {
+                        gameBoard[currentRow][currentColumn] = currentPlayer;
+                        log(gameBoard)
+                        currentPlayer = (currentPlayer === 'x') ? '0' : 'x';
+                    }
+                });
+            });
+        });
+    });
 })();
 
 const DisplayController = (() => {
@@ -28,12 +52,10 @@ const DisplayController = (() => {
         DomGrabber.gameContainer.classList.remove('hidden');
         DomGrabber.form.reset();
     });
-    
 })();
 
 const Player = (name) => {
-    const playerName = name;
     return {
-        name: playerName
+        name
     }
 }
