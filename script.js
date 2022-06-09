d4e3c98273dd51cca97bc287be4e326d1828536e
JoaoMateus98 <joaobsu98@gmail.com>
2022-06-09 17:52:02 -0400

    On start button press-create players and hide start game div

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const log = (...args) => console.log(...args);
+let playerOne;
+let playerTwo;
 
-const gameBoard = (() => {
+const DomGrabber = (() => {
+    const startGameContainer = document.querySelector('.start-game-container');
+    const gameContainer = document.querySelector('.game-container');
     const startButton = document.querySelector('.start-button');
+    const form = document.querySelector('form');
+
+    return {
+        startButton,
+        form,
+        startGameContainer,
+        gameContainer
+    }
+})();
+
+const GameBoard = (() => {
+    DomGrabber.startButton.addEventListener('click', () => {
+        let formData = new FormData(DomGrabber.form);
+        playerOne = Player(formData.get('player1'));
+        playerTwo = Player(formData.get('player2'));
+        DomGrabber.startGameContainer.classList.add('hidden');
+        DomGrabber.gameContainer.classList.remove('hidden');
+    });
+})();
+
+const DisplayController = (() => {
     
 })();
 
-const displayController = (() => {
-
-})();
-
-const player = (name) => {
-
+const Player = (name) => {
+    let playerName = name;
+    return {
+        name: playerName
+    }
 }
