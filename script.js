51bbe3aeea658f6ca7d1e3de758e875039215d37
JoaoMateus98 <joaobsu98@gmail.com>
2022-06-10 08:25:03 -0400

    fixed-diagonal winning condition not working

--- script.js
+++ script.js
@@ -72,7 +72,8 @@ const checkWinner = () => {
         return currentPlayer;
     }
     if (gameBoard[1][1] === gameBoard[0][1] && gameBoard[1][1] === gameBoard[2][1] ||
-        gameBoard[1][1] === gameBoard[1][0] && gameBoard[1][1] === gameBoard[1][2]) {
+        gameBoard[1][1] === gameBoard[1][0] && gameBoard[1][1] === gameBoard[1][2] ||
+        gameBoard[1][1] === gameBoard[0][2] && gameBoard[1][1] === gameBoard[0][2]) {
         return currentPlayer;
     }
 
